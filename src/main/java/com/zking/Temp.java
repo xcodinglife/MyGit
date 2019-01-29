cbc8716d923f574d28967375ee2e8a51a52b2ce4
xiexielucks <xyqlucks@163.com>
2019-01-29 22:08:11 +0800

    yes

--- src/main/java/com/zking/Temp.java
+++ src/main/java/com/zking/Temp.java
@@ -3,9 +3,7 @@ package com.zking;
 public class Temp {
 
     public static void main(String[] args) {
-        System.out.println("git......");
-        System.out.println("git2......");
-        System.out.println("git3......");
+        System.out.println("你是猪");
     }
 
 
