51213d2644ef8be523f154f73a9af2c06beba329
xcodinglife <xcodinglife@qq.com>
2019-01-29 19:43:16 +0800

    需要commit才能提交

---
+++ src/main/java/com/zking/MygitApplication.java
@@ -0,0 +1,14 @@
+package com.zking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygitApplication.class, args);
+	}
+
+}
+
